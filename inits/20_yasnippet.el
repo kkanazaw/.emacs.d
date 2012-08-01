af78daf850d70091d6930fef6327c317334305ee
malmrashede <new.frontier2006@gmail.com>
2012-08-01 13:54:30 +0900

    change snippet directory config

--- inits/20_yasnippet.el
+++ inits/20_yasnippet.el
@@ -2,6 +2,6 @@
 (require 'yasnippet)
 (require 'anything-c-yasnippet)
 (yas/initialize)
-(yas/load-directory "/home/sanshiro/.emacs.d/elisp/yasnippet/snippets/")
+(yas/load-directory "~/.emacs.d/elisp/yasnippet/snippets/")
 (setq yas/prompt-functions '(yas/dropdown-prompt))
 (yas/global-mode 1)
