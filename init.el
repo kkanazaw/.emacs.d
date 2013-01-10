8c00aaa0480b2a9419bd9bb84c1e35b66f589756
malmrashede <new.frontier2006@gmail.com>
2013-01-10 19:34:54 +0900

    update howm 1.4.1

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/mmm-mode")
 (add-to-list 'load-path "~/.emacs.d/elisp/magit")
 (add-to-list 'load-path "~/.emacs.d/elisp/emacs-w3m/share/emacs/site-lisp/w3m/")
+(add-to-list 'load-path "~/.emacs.d/elisp/org")
 ;(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
 ;(add-to-list 'load-path "~/.emacs.d/rhtml")
 ;(add-to-list 'load-path "~/.emacs.d/scala")
@@ -26,8 +27,14 @@
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits") ; 設定ファイルがあるディレクトリを指定
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
