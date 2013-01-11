b519ade6d193c185b6e0cd7d1799e436c4979712
U-YAHOO\kkanazaw <kkanazaw@NI65P-123.yjoffice.local>
2013-01-11 11:04:21 +0900

    change background color for transparent

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "color-239" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
