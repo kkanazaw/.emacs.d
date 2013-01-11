423fa1b82ae1df726457a3430b87c899e3ef2f4d
U-YAHOO\kkanazaw <kkanazaw@NI65P-123.yjoffice.local>
2013-01-11 10:30:28 +0900

    * change org-mode,agenda directory for office

---
+++ inits/60_org_office.el
@@ -0,0 +1,7 @@
+(setq org-directory "~/My Documents/howm/")
+;; org-default-notes-fileのファイル名
+(setq org-default-notes-file (concat org-directory "todo.txt"))
+(setq org-agenda-files (list org-directory))
+
+;; アジェンダ表示の対象ファイル
+(setq org-agenda-files (quote ("~/My Documents/howm/todo.txt")))
